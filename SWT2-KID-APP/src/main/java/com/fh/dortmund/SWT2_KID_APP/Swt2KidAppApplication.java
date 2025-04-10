b34b127c2a7c35b3bbdcd21bf4df5917b0fdf67b
Fotso Joy <joyfotso1@gmail.com>
2025-04-10 15:24:30 +0200

    First commit

---
+++ SWT2-KID-APP/src/main/java/com/fh/dortmund/SWT2_KID_APP/Swt2KidAppApplication.java
@@ -0,0 +1,13 @@
+package com.fh.dortmund.SWT2_KID_APP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swt2KidAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Swt2KidAppApplication.class, args);
+	}
+
+}
---
+++ SWT2-KID-APP/src/test/java/com/fh/dortmund/SWT2_KID_APP/Swt2KidAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fh.dortmund.SWT2_KID_APP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swt2KidAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
