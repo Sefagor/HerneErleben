af20f67422175c977b7d827eee8b9f23afef7d7f
sefa <sefagor5525@gmail.com>
2025-05-10 14:05:40 +0200

    feat(frontend): basic Home page

---
+++ frontend_app/src/components/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Kids App!</h1>
+      <p>This is our Home Page.</p>
+    </div>
+  );
+};
+
+export default HomePage;
