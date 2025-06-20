8e1bac0112a1d70892ccfeea79700f507bd28fad
Sefa Gör <sefagor5525@gmail.com>
2025-06-20 13:16:30 +0200

    Event Details Seite und einige Bug fixes

---
+++ frontend_app/src/components/common/Pagination.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const Pagination = ({
+                        roomsPerPage,
+                        totalRooms,
+                        itemsPerPage,
+                        totalItems,
+                        currentPage,
+                        paginate
+                    }) => {
+    const perPage = roomsPerPage || itemsPerPage;
+    const total = totalRooms || totalItems;
+    const pageNumbers = [];
+
+    for (let i = 1; i <= Math.ceil(total / perPage); i++) {
+        pageNumbers.push(i);
+    }
+
+    return (
+        <div className='pagination-nav'>
+            <ul className="pagination-ul">
+                {pageNumbers.map((number) => (
+                    <li key={number} className="pagination-li">
+                        <button
+                            onClick={() => paginate(number)}
+                            className={`pagination-button ${currentPage === number ? 'current-page' : ''}`}
+                        >
+                            {number}
+                        </button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default Pagination;
